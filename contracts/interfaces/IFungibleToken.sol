3a5fff26753b2c3da1e67e5dce9394f390ea190f
pironmind <paulbolhaskih@yahoo.com>
2022-03-31 15:03:05 +0300

    inherit IFungibleToken interface from IERC20

--- contracts/interfaces/IFungibleToken.sol
+++ contracts/interfaces/IFungibleToken.sol
@@ -2,7 +2,9 @@
 
 pragma solidity >=0.8.0 <1.0.0;
 
-interface IFungibleToken {
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IFungibleToken is IERC20 {
     function mint(address to, uint256 amount) external;
     function burn(uint256 amount) external;
 }
