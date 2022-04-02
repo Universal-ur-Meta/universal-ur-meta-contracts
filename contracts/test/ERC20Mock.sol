790adf6892316beaabbd552138b9d806dc3b4e09
pironmind <paulbolhaskih@yahoo.com>
2022-04-02 09:34:46 +0300

    add test contracts

---
+++ contracts/test/ERC20Mock.sol
@@ -0,0 +1,11 @@
+/// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Mock is ERC20("Mock ERC20", "E20MOCK") {
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
