2fe7f0ce6f379f9537046068bc46a195c1230727
pironmind <paulbolhaskih@yahoo.com>
2022-04-02 09:34:36 +0300

    add test Helper

---
+++ test/Helper.ts
@@ -0,0 +1,20 @@
+import { expect } from "chai";
+import { network } from "hardhat";
+
+export function getTimestamp() {
+    return Math.floor(Date.now() / 1000);
+}
+
+export async function expectRevert(condition: any, message: string) {
+    await expect(condition).to.revertedWith(message);
+}
+
+export async function increaseTime(forSeconds: number) {
+    await network.provider.send("evm_increaseTime", [forSeconds])
+    await network.provider.send("evm_mine")
+}
+
+export async function setNextBlockTimestamp(timestamp: number) {
+    await network.provider.send("evm_setNextBlockTimestamp", [timestamp])
+    await network.provider.send("evm_mine")
+}
